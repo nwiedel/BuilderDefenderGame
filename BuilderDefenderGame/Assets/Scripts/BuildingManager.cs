3dc739037eb5d68f2a27abd3ae9b6280329af711
nwiedel <nwiedel@alte-hof-apotheke.com>
2024-11-05 17:36:10 +0100

    BuildingTypeList

--- BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
+++ BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
@@ -7,7 +7,11 @@ public class BuildingManager : MonoBehaviour
     /// <summary>
     /// Verweis auf das Prefa
     /// </summary>
-    [SerializeField] private Transform pfWoodHarvester;
+    private BuildingTypeSO buildingType;
+    /// <summary>
+    /// Verweis auf die Liste der BuildingTpes
+    /// </summary>
+    private BuildingTypeListSO buildingTypeList;
 
     /// <summary>
     /// Referenz auf die MainCamera
@@ -17,13 +21,25 @@ public class BuildingManager : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main;
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        buildingType = buildingTypeList.list[0];
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            buildingType = buildingTypeList.list[0];
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            buildingType = buildingTypeList.list[1];
         }
     }
 
