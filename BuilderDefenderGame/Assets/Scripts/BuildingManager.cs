ed65595f8eb79f857be3ec3b1dfa1f47e9fe11cd
nwiedel <nwiedel@alte-hof-apotheke.com>
2024-11-11 18:18:27 +0100

    Generating Resources 1

--- BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
+++ BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
@@ -18,12 +18,15 @@ public class BuildingManager : MonoBehaviour
     /// </summary>
     private Camera mainCamera;
 
+    private void Awake()
+    {
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        buildingType = buildingTypeList.list[0];
+    }
+
     private void Start()
     {
         mainCamera = Camera.main;
-
-        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
-        buildingType = buildingTypeList.list[0];
     }
 
     private void Update()
--- BuilderDefenderGame/Assets/Scripts/ResourceManager.cs
+++ BuilderDefenderGame/Assets/Scripts/ResourceManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Stellt die Menge der Resourcen nach Art dar.
+    /// </summary>
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+    private void Awake()
     {
-        
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            resourceAmountDictionary[resourceType] = 0;
+        }
+
+        TestLogResourceAmountDictionary();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+            TestLogResourceAmountDictionary();
+        }
+    }
+
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach(ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]); 
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
     }
 }
